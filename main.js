eeb5a4ed8216ea12b4348c3a57e9968756c819dc
lomowang <frees1102@gmail.com>
2023-03-01 00:26:46 +0800

    20230301TV增加完成

--- main.js
+++ main.js
@@ -3,7 +3,11 @@
 // APIURL
 const API_KEY = 'api_key=5d7ac24e63419994fda11db9f90c8f2b';
 const BASE_URL ='https://api.themoviedb.org/3';
+// 電影的api
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+// 影集的api
+const API_TVURL = BASE_URL + '/tv/popular?&'+API_KEY;
+
 const IMG_URL= 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL+'/search/movie?'+API_KEY;
 const genres = [
@@ -92,14 +96,15 @@ const genres = [
 
 
 const main = document.getElementById('main');
+console.log(main)
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 const tagsEL = document.getElementById('tags');
+console.log(tags)
 const header = document.getElementById('header');
 
-
-const mainTV = document.getElementById('mainTV');
-
+const trendingTvshow = document.getElementById('trendingTvshow');
+console.log(trendingTvshow)
 
 
 
@@ -152,7 +157,7 @@ function setGenre(){
   })
 }
 
-
+// 標籤
 function highlightSelection(){
   // 移除標籤變色
   const tag = document.querySelectorAll('.tag');
@@ -191,6 +196,7 @@ function clearBtn(){
 }
 
 
+// 這是秀出電影的
 // main段落的
 getMovies(API_URL);
 
@@ -204,6 +210,8 @@ function getMovies(url){
         }
     })
 }
+
+
 
 // main段落的
 function showMovies(data){
@@ -225,6 +233,7 @@ function showMovies(data){
         </div>
         `
         main.appendChild(movieEL);
+        console.log(main)
 
         })
         
@@ -232,122 +241,48 @@ function showMovies(data){
     
   
 
- 
+// 這是秀出影集的
+
+gettvshow(API_TVURL);
+
+function gettvshow(url) {
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.results);
+      if (data.results.length !== 0) {
+        tvshow(data.results);
+      } else {
+        trendingTvshow.innerHTML = `<h1 class="no-results">No Results</h1>`;
+      }
+    });
+}
 
 
-    
-    // const overlayContent = document.getElementById('overlay-content');
-    /* Open when someone clicks on the span element */
-    // function openNav(movie) {
-    //   let id = movie.id;
-    //     fetch(BASE_URL + '/movie/'+id+'/videos?'+API_KEY).then(res => res.json())
-    //     .then(videoData =>{
-    //         // 2.抓到ID後幫我印出值的資料
-            // console.log(videoData);
-            // if(videoData){
-            //     document.getElementById("myNav").style.width = "100%";
-            // if(videoData.results.length > 0){
-            //     // 設定一個空字串 裝movievideo
-                // var embed = [];
-                // var dots = [];
-                // videoData.results.forEach((video, idx)=>{
-                //     // 設定要抓取的值
-                    // let {name, key, site} = video
-                    // if(site == 'YouTube'){
-                    //   embed.push(`
-                    //    <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                  
-                    //   `)
+function tvshow(data) {
+  trendingTvshow.innerHTML = '';
 
 
-      //                 dots.push(`
-      //                 <span class="dot">${idx + 1}</span>
-      //                 `)
-      //               }
-    
-      //           })
+data.forEach(tv =>{        
+  const {poster_path, first_air_date, vote_average ,name } = tv;
+  const trendingTvElement = document.createElement('div');
+  trendingTvElement.classList.add('tv');
+  trendingTvElement.innerHTML=`
+  <img src="${IMG_URL+poster_path}" alt="${name}">
+      <div class="movieInfo">
+          <h3>${name}</h3>
+      </div>
+      <p class="green">${vote_average}</p>
+      <div class="date">
+          <p >${first_air_date}</p>
+      </div>
+  </div>
+  `
+  trendingTvshow.appendChild(trendingTvElement);
+  console.log(trendingTvshow)
 
-      //           var content =`
-      //           <h1 class="no-results">${movie.original_title}</h1>
-      //           <br/>
-
-      //           ${embed.join('')}
-      //           <br/>
-
-      //           <div class="dots">${dots.join('')}</div>
-      //           `
-      //           overlayContent.innerHTML = content;
-      //           activeSlide=0;
-      //           showVideos();
-      //       }else{
-      //           overlayContent.innerHTML = `<h1 class="no-results">No Results Found</h1>`
-    
-      //       }
-      //     }
-      //   })
-      // }
-
-      /* Close when someone clicks on the "x" symbol inside the overlay */
-      // function closeNav() {
-      //   document.getElementById("myNav").style.width = "0%";
-      // }
-
-      // // 展示一個影片
-      // var activeSlide = 0;
-      // var totalVideos = 0;
-
-      // function showVideos(){
-      //   let embedClasses = document.querySelectorAll('.embed');
-      //   let dots = document.querySelectorAll('.dot');
-
-
-      //   totalVideos = embedClasses.length;
-      //   embedClasses.forEach((embedTag, idx) => {
-      //     if(activeSlide == idx){
-      //       embedTag.classList.add('show')
-      //       embedTag.classList.remove('hide')
-          
-      //     }else{
-      //       embedTag.classList.add('hide');
-      //       embedTag.classList.remove('show')
-            
-      //     }
-
-      //   })
-
-
-      //   dots.forEach((dot, indx) => {
-      //     if(activeSlide == indx){
-      //       dot.classList.add('active');
-      //     }else{
-      //       dot.classList.remove('active')
-      //     }
-      //   })
-      // }
-      // const leftArrow = document.getElementById('left-arrow')
-      // const rightArrow = document.getElementById('right-arrow')
-      
-      // leftArrow.addEventListener('click', () => {
-      //   if(activeSlide > 0){
-      //     activeSlide--;
-      //   }else{
-      //     activeSlide = totalVideos -1;
-      //   }
-      
-      //   showVideos()
-      // })
-
-
-      
-      // rightArrow.addEventListener('click',() => {
-      //   if(activeSlide < (totalVideos -1)){
-      //     activeSlide++;
-      //   }else{
-      //     activeSlide = 0;
-      //   }
-      //   showVideos()
-      // })
-
+  })
+}
 
 
 
